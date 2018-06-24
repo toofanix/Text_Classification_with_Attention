c33717d0108548d2d017ef34d97bf9444e8ce109
toofanix <jigarbandaria@gmail.com>
2018-06-24 13:50:59 -0400

    Initial function for classification

---
+++ text_classification_covnet.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+import re
+import typing
+from bs4 import BeautifulSoup
+
+import sys
+import os
+
+os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
+
+from keras.layers import Embedding
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Merge, Dropout
+from keras.models import Model
+
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NB_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+def clean_str(string: str) -> str:
+	'''
+	Tokenization/string cleaning for dataset
+	Every dataset is lower cased except
+	:param string: String to be tokenized can cleaned
+	:return: tokenized/cleaned string
+	'''
+	string = re.sub(r"\\", "", string)
+	string = re.sub(r"\'", "", string)
+	string = re.sub(r"\"", "", string)
+	return string.strip().lower()
+
