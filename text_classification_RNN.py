b7a643099d4b56484831afc8ae0688a5d19a46c0
toofanix <jigarbandaria@gmail.com>
2018-06-29 22:29:48 -0400

    import modules.

--- text_classification_RNN.py
+++ text_classification_RNN.py
@@ -15,3 +15,13 @@ DIR_PATH = os.getcwd()
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils.np_utils import to_categorical
+
+from keras.layers import Embedding
+from keras.layers import Dense, Input, Flatten
+from keras.layers import Conv1D, MaxPooling1D, Merge, Dropout
+from keras.layers import LSTM, GRU, Bidirectional
+from keras.models import Model
+
+from keras import backend as K
+from keras.engine.topology import Layer, InputSpec
+from keras import initializers
