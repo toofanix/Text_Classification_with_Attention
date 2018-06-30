3526919608fe2a1e0b8652b93f5d237c12f6f126
toofanix <jigarbandaria@gmail.com>
2018-06-30 10:13:38 -0400

    create attention layer class

--- text_classification_RNN.py
+++ text_classification_RNN.py
@@ -140,3 +140,27 @@ print('Model using a complex convolutional neural network :')
 model.summary()
 
 model.fit(x_train, y_train, epochs=20, validation_data=(x_valid, y_valid), batch_size=50)
+
+# Attention GRU network
+class AttentionLayer(Layer):
+	def __init__(self, **kwargs):
+		self.init = initializers.get('normal')
+		super(AttentionLayer, self).__init__(**kwargs)
+
+	def build(self, input_shape):
+		assert len(input_shape) == 3
+		self.W = self.init((input_shape[-1],))
+		self.trainable_weights = [self.W]
+		super(AttentionLayer, self).build(input_shape)
+
+	def call(self, x, mask=None):
+		eij = K.tanh(K.dot(x, self.W))
+		ai = K.exp(eij)
+		weights = ai/K.sum(ai, axis=1).dimshuffle(0, 'x')
+		weighted_input = x * weights.dimshuffle(0, 1, 'x')
+		return weighted_input.sum(axis=1)
+
+	def get_output_shape_for(self, input_shape):
+		return(input_shape[0], input_shape[-1])
+
+
